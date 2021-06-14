5ba8f3b1a07d30a07aba704ef94f3cbdc16ef7c3
Sebastian Kürten <sebastian@topobyte.de>
2021-06-14 11:39:35 +0200

    Copy hydrants map file to app project

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -6,3 +6,4 @@ ASSETS="$REPO/../app/app/src/main/assets/"
 
 cp "$REPO/Brandenburg.xmap" "$ASSETS/map.xmap.jet"
 cat "$REPO/Brandenburg.sqlite" | gzip > "$ASSETS/map.sqlite.jet"
+cp "$REPO/Brandenburg-hydrants.xmap" "$ASSETS/hydrants.xmap.jet"
