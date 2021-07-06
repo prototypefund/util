4e32c445738599a9da3fdbb69384236b194e4d64
Sebastian Kürten <sebastian@topobyte.de>
2021-07-06 09:33:48 +0200

    Use regions when building search database

--- scripts/create-database.sh
+++ scripts/create-database.sh
@@ -6,8 +6,9 @@ TBO="$HOME/github/waldbrand/osm-data/Brandenburg.tbo"
 BOUNDARY="$REPO/data/Brandenburg.smx"
 TMP="$REPO/tmp"
 CONFIG="$REPO/pois.xml"
+REGIONS="$REPO/data/regions"
 
 ~/github/topobyte/nomioc/setup-cli/scripts/NomiocDatabaseCreatorCustom \
-    --input "$TBO" --boundary "$BOUNDARY" \
+    --input "$TBO" --boundary "$BOUNDARY" --regions "$REGIONS" \
     --node-db "$TMP/nodes" --way-db "$TMP/ways" \
     --config "$CONFIG" --output "$REPO/Brandenburg.sqlite"
